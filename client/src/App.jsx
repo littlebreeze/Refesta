de966ffc66e2770fa07c51534f5ac14bbfd818de
권정훈 <dhtmxk8134@naver.com>
2024-03-12 09:05:49 +0900

    💄 design(fe): App 컴포넌트 UI 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className='App'>
-        <h1>REFESTA</h1>
-        <h2>test</h2>
-      </div>
-    </>
+    <div className='App'>
+      <h1>REFESTA</h1>
+    </div>
   );
 }
 
