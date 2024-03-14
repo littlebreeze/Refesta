b2c895d069601621e65c62a38aea9a35225acb02
이지원 <dlwldnjs950@naver.com>
2024-03-14 21:24:59 +0900

    ✨ feat(fe): 구글 로그인 시작

--- client/src/components/start/Google_login.jsx
+++ client/src/components/start/Google_login.jsx
@@ -1,7 +1,10 @@
 import { useSearchParams } from 'react-router-dom';
 const Google_Login = () => {
   const [params, setParams] = useSearchParams();
-  console.log(params.get('code'));
+  const code = params.get('code');
+  // params.get('code')에 담겨있고
+  // 이걸 백으로 보내서 isSignUp 여부로
+  // 페이지 이동 -> Home 또는 프로필 편집
 
   return <h2>code 가져오기</h2>;
 };
