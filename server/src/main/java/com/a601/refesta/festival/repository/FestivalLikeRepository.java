ca0f80fee2fae1a4feb4cddfef8fddb13eafc322
이경배 <foofoo964@naver.com>
2024-03-14 17:57:18 +0900

    📝 style: FestivalLike 조회  파라미터 순서 변경

---
+++ server/src/main/java/com/a601/refesta/festival/repository/FestivalLikeRepository.java
@@ -0,0 +1,10 @@
+package com.a601.refesta.festival.repository;
+
+import com.a601.refesta.member.domain.join.FestivalLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FestivalLikeRepository extends JpaRepository<FestivalLike, Integer> {
+    Optional<FestivalLike> findByFestival_IdAndMember_Id(String memberId, int fetivalId);
+}
--- server/src/main/java/com/a601/refesta/festival/service/FestivalService.java
+++ server/src/main/java/com/a601/refesta/festival/service/FestivalService.java
@@ -6,13 +6,17 @@ import com.a601.refesta.festival.data.FestivalInfoRes;
 import com.a601.refesta.festival.domain.Festival;
 import com.a601.refesta.festival.domain.FestivalDetail;
 import com.a601.refesta.festival.repository.FestivalDetailRepository;
+import com.a601.refesta.festival.repository.FestivalLikeRepository;
 import com.a601.refesta.festival.repository.FestivalRepository;
+import com.a601.refesta.member.domain.join.FestivalLike;
+import com.a601.refesta.member.service.MemberService;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.a601.refesta.festival.domain.QFestival.festival;
 import static com.a601.refesta.festival.domain.QFestivalDetail.festivalDetail;
@@ -21,8 +25,12 @@ import static com.a601.refesta.festival.domain.QFestivalDetail.festivalDetail;
 @RequiredArgsConstructor
 public class FestivalService {
 
+    private final MemberService memberService;
+
     private final FestivalRepository festivalRepository;
     private final FestivalDetailRepository festivalDetailRepository;
+    private final FestivalLikeRepository festivalLikeRepository;
+
     private final JPAQueryFactory jpaQueryFactory;
 
     /**
@@ -33,6 +41,7 @@ public class FestivalService {
     public FestivalInfoRes getFestivalInfo(int festivalId) {
         Festival findFestival = getFestival(festivalId);
 
+        //기본 정보 저장
         FestivalInfoRes festivalInfo = FestivalInfoRes.builder()
                 .name(findFestival.getName())
                 .date(findFestival.getDate())
@@ -40,6 +49,7 @@ public class FestivalService {
                 .posterUrl(findFestival.getPosterUrl())
                 .build();
 
+        //예정된 페스티벌이면, 세부 정보 추가
         if (!findFestival.isEnded()) {
             FestivalDetail findDetail = festivalDetailRepository.findByFestival_Id(festivalId)
                     .orElseThrow(() -> new CustomException(ErrorCode.FESTIVAL_DETAIL_NOT_FOUND_ERROR));
@@ -48,6 +58,33 @@ public class FestivalService {
         }
 
         return festivalInfo;
+    }
+
+    /**
+     * 페스티벌 좋아요 업데이트
+     * @param memberId - 구글 식별 ID
+     * @param festivalId
+     */
+    public void updateFestivalLike(String memberId, int festivalId) {
+        Optional<FestivalLike> optFindLike = festivalLikeRepository.findByFestival_IdAndMember_Id(memberId, festivalId);
+
+        //DB에 없으면 추가
+        if(optFindLike.isEmpty()) {
+            festivalLikeRepository.save(FestivalLike.builder()
+                    .member(memberService.getMember(memberId))
+                    .festival(getFestival(festivalId))
+                    .isLiked(true)
+                    .build()
+            );
+
+            return;
+        }
+
+        //DB에 있으면 좋아요 상태 업데이트 후 저장
+        FestivalLike findLike = optFindLike.get();
+        findLike.updateStatus();
+
+        festivalLikeRepository.save(findLike);
     }
 
     public Festival getFestival(int festivalId) {
