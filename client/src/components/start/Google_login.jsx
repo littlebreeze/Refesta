3e2148a9d6852fe4acd2f898cbcb76471f95bb19
이지원 <dlwldnjs950@naver.com>
2024-03-15 11:36:41 +0900

    ✨ feat(fe): 로그인 흐름 정리

--- client/src/components/start/Google_login.jsx
+++ client/src/components/start/Google_login.jsx
@@ -1,10 +1,46 @@
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 const Google_Login = () => {
+  const nav = useNavigate();
+
+  const handleHome = () => {
+    nav('/', { replace: true });
+    //window.location.reload();
+  };
+
+  const handleProfile = () => {
+    nav('/nickname', { replace: true });
+    //window.location.reload();
+  };
+
   const [params, setParams] = useSearchParams();
   const code = params.get('code');
-  // params.get('code')에 담겨있고
-  // 이걸 백으로 보내서 isSignUp 여부로
-  // 페이지 이동 -> Home 또는 프로필 편집
+
+  const handleLoginPost = async (code) => {
+    // code url에서 받아오기
+    const data = { code: code };
+    try {
+      // 백으로 요청 보내기
+      const res = await fetch(
+        'https://jsonplaceholder.typicode.com/comments'
+      ).then((res) => res.json());
+
+      // 토큰 저장
+      // isSigUp으로 기존/신규 여부 판단
+      res ? handleHome() : handleProfile();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (code) {
+      handleLoginPost(code);
+    } else {
+      console.log('로그인 재시도하세요.');
+    }
+  }, [code, nav]);
 
   return <h2>code 가져오기</h2>;
 };
