ba9faff107aaa897c572d85c3ae01cfe63576ad9
이경배 <foofoo964@naver.com>
2024-03-18 17:33:40 +0900

    📝 style(be): FestivalLike 패키지 member로 이동

--- server/src/main/java/com/a601/refesta/member/repository/FestivalLikeRepository.java
+++ server/src/main/java/com/a601/refesta/member/repository/FestivalLikeRepository.java
@@ -1,4 +1,4 @@
-package com.a601.refesta.festival.repository;
+package com.a601.refesta.member.repository;
 
 import com.a601.refesta.member.domain.join.FestivalLike;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FestivalLikeRepository extends JpaRepository<FestivalLike, Integer> {
-    Optional<FestivalLike> findByMember_IdAndFestival_Id(String memberId, int fetivalId);
+    Optional<FestivalLike> findByMember_GoogleIdAndFestival_Id(String memberId, int festivalId);
 }
