aa04c10a39a512118c288283fc2fe9ca001553ae
이지원 <dlwldnjs950@naver.com>
2024-03-18 16:59:25 +0900

    ✏️ rename(fe): pages/RegisterInfo.jsx -> pages/RegisterInfoPage.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/LoginPage';
-import RegisterInfo from './pages/RegisterInfo';
+import RegisterInfo from './pages/RegisterInfoPage';
 import Home from './pages/Home';
 import FestivalList from './pages/FestivalList';
 import FestivalDetail from './pages/FestivalDetail';
