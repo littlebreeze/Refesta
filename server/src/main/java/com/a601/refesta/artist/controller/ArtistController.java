18d2d4eb82af496d673ef86d912cd9e730e054fb
이경배 <foofoo964@naver.com>
2024-03-19 12:59:03 +0900

    ✨ feat(be): 아티스트 정보 조회, 좋아요 업데이트 컨트롤러에 추가

--- server/src/main/java/com/a601/refesta/artist/controller/ArtistController.java
+++ server/src/main/java/com/a601/refesta/artist/controller/ArtistController.java
@@ -1,10 +1,10 @@
 package com.a601.refesta.artist.controller;
 
+import com.a601.refesta.artist.data.ArtistInfoRes;
 import com.a601.refesta.artist.service.ArtistService;
 import com.a601.refesta.common.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,7 +15,13 @@ public class ArtistController {
 
     private final ArtistService artistService;
 
-    public SuccessResponse<Void> editArtistLike(String memberId, List<Integer> artistIdList) {
+    @GetMapping("/{artist_id}")
+    public SuccessResponse<ArtistInfoRes> getArtistInfo(@PathVariable(name = "artist_id") int artistId) {
+        return new SuccessResponse<>(artistService.getArtistInfo(artistId));
+    }
+
+    @PatchMapping
+    public SuccessResponse<Void> editArtistLike(String memberId, @RequestBody List<Integer> artistIdList) {
         //JWT 코드 추가 필요
         artistService.updateArtistLike(memberId, artistIdList);
         return new SuccessResponse<>(null);
