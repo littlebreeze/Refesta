e50077039b296351f2d3f2e6d56ecc70f05c8b9b
권정훈 <dhtmxk8134@naver.com>
2024-03-19 17:49:28 +0900

    💄 design(fe): 헤더 UI 수정 & 라우팅

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,28 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import text_logo from '../../assets/text_logo.png';
+import search from '../../assets/search.png';
+import menu from '../../assets/menu.png';
 
 const Header = () => {
   return (
-    <header className="bg-white-800 py-4">
-      <div className="container mx-auto flex items-center justify-between">
-        {/* 로고 */}
-        <div className="flex items-center">
-          <img src={text_logo} alt="" className="h-9 w-20" />
-        </div>
-        
+    <header className='py-4 bg-white-800'>
+      <div className='container flex items-center justify-between mx-auto'>
         {/* 검색 버튼 */}
-        <div className="flex-grow text-center">
-          <button className="bg-white text-gray-800 px-4 py-2 rounded-full shadow-md hover:bg-gray-200">
-            검색
-          </button>
+        <div className='flex text-center'>
+          <Link to='/search' className='cursor-pointer'>
+            <img src={search} alt='Logo' className='w-5 h-5 ml-5' />
+          </Link>
         </div>
-        
+
+        {/* 로고 */}
+        <div className='flex items-center'>
+          <Link to='/' className='cursor-pointer'>
+            <img src={text_logo} alt='Logo' className='w-20 h-9' />
+          </Link>
+        </div>
+
         {/* 햄버거 버튼 */}
-        <div className="flex items-center bg-red-800">
-          <button className="text-white focus:outline-none">
-            <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
-          </button>
+        <div className='flex items-center'>
+          <img src={menu} alt='Logo' className='w-5 h-5 mr-5' />
         </div>
       </div>
     </header>
