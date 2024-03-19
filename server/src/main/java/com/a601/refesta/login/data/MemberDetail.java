8a80237af867f3f956ffaa4f1a1a1f8b35236e2f
최정윤 <janet971015@naver.com>
2024-03-19 14:17:19 +0900

    🔨 refactor(be): Getter 추가

--- server/src/main/java/com/a601/refesta/login/data/MemberDetail.java
+++ server/src/main/java/com/a601/refesta/login/data/MemberDetail.java
@@ -1,8 +1,10 @@
 package com.a601.refesta.login.data;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class MemberDetail {
     private String googleId;
     private String email;
