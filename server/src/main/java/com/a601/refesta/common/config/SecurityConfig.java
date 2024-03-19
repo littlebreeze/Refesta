c54b269e93bef93ad8c3daa5f3bc16aa396269b4
최정윤 <janet971015@naver.com>
2024-03-19 14:16:24 +0900

    ✨ feat(be): filter 구현

--- server/src/main/java/com/a601/refesta/common/config/SecurityConfig.java
+++ server/src/main/java/com/a601/refesta/common/config/SecurityConfig.java
@@ -4,8 +4,13 @@ import com.a601.refesta.common.jwt.JwtAccessDeniedHandler;
 import com.a601.refesta.common.jwt.JwtAuthenticationEntryPoint;
 import com.a601.refesta.common.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +24,20 @@ public class SecurityConfig {
     private final JwtFilter jwtFilter;
 
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
+        http.exceptionHandling(authEntryPoint -> authEntryPoint
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler));
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(
+                request -> request.requestMatchers("")
+                        .permitAll()
+                        .anyRequest().authenticated());//로그인 주소만 허용하면 된다.
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
 }
--- server/src/main/java/com/a601/refesta/common/jwt/JwtFilter.java
+++ server/src/main/java/com/a601/refesta/common/jwt/JwtFilter.java
@@ -5,6 +5,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -12,6 +15,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@Slf4j //log 사용
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private static final String AUTHORIZATION_HEADER = "Authorization";
@@ -23,9 +27,16 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
 
-        String token=resolveToken(request);
-        tokenProvider.validateToken(token);
+        String token = resolveToken(request);
 
+        if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.info("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+        } else {
+            log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+        }
+        filterChain.doFilter(request, response);
     }
 
 
