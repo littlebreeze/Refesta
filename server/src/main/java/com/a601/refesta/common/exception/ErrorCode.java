67f2392983dd6b014f08b84688a42d0dcea036c0
최정윤 <janet971015@naver.com>
2024-03-19 14:10:04 +0900

    ✨ feat(be): RefreshToken 에러 추가

--- server/src/main/java/com/a601/refesta/common/exception/ErrorCode.java
+++ server/src/main/java/com/a601/refesta/common/exception/ErrorCode.java
@@ -13,6 +13,9 @@ public enum ErrorCode {
     //Token
     ACCESS_TOKEN_EXPIRE_ERROR(401,"TOK001","ACCESS TOKEN이 만료되었습니다."),
     ACCESS_TOKEN_ERROR(401, "TOK002", "Access Token이 잘못되었습니다."),
+    REFRESH_TOKEN_VALIDATION_ERROR(401, "TOK003", "Refresh Token이 잘못되었습니다."),
+
+
     //Festival
     FESTIVAL_NOT_FOUND_ERROR(404, "FES001", "존재하지 않는 페스티벌입니다."),
     FESTIVAL_DETAIL_NOT_FOUND_ERROR(500, "FES002", "예정된 페스티벌의 상세 정보가 존재하지 않습니다."),
