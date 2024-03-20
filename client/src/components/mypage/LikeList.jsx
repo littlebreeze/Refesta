a6d6a797002ba95e481ee6c184489d7ec182b125
하재률 <wofbf2@gmail.com>
2024-03-20 16:18:25 +0900

    💄 design(fe): 좋아요 목록 페이지 UI 구현

--- client/src/components/mypage/LikeList.jsx
+++ client/src/components/mypage/LikeList.jsx
@@ -1,5 +1,21 @@
-const LikeList = () => {
-  return <h2>LikeList</h2>;
+import LikeItem from './../mypage/LikeItem';
+
+const LikeList = ({ contents }) => {
+  console.log(contents);
+  return (
+    <div className="grid grid-cols-2 gap-3 pt-3 mx-3 bg-gray-200">
+      {contents && contents.length > 0 ? (
+        contents.map((content) => (
+          <LikeItem
+            key={content.id}
+            artist={content}
+          />
+        ))
+      ) : (
+        <div className="mt-10 text-xl font-bold text-center">좋아하는 아티스트가 없어요</div>
+      )}
+    </div>
+  );
 };
 
 export default LikeList;
