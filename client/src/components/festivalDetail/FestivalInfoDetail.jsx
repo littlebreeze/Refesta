fe522f25dc42a26bdacbb7591c105cd82415ef14
권정훈 <dhtmxk8134@naver.com>
2024-03-20 17:55:52 +0900

    ✨ feat(fe): 플레이어 기능 구현

--- client/src/components/festivalDetail/FestivalInfoDetail.jsx
+++ client/src/components/festivalDetail/FestivalInfoDetail.jsx
@@ -1,10 +1,8 @@
 const FestivalInfoDetail = () => {
   return (
     <>
-      <img
-        src='https://mintpaper.s3.ap-northeast-2.amazonaws.com/78_LIVE_New_Link_f1797dd7d9.jpg'
-        alt=''
-      />
+      <img src='https://mintpaper.s3.ap-northeast-2.amazonaws.com/78_LIVE_New_Link_f1797dd7d9.jpg' alt='' />
+      <div className='py-10'></div>
     </>
   );
 };
---
+++ client/src/components/festivalDetail/_ReactPlayer.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { useState, useRef } from 'react';
+import ReactPlayer from 'react-player';
+
+// const _ReactPlayer = ({ props }) => {
+const _ReactPlayer = () => {
+  // const { playing, setPlaying, playlist } = props; // 상위 컴포넌트에 playing, setPlaying true로 정의
+  // const playerRef = useRef(null);
+  const [ready, setReady] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [curr, setCurr] = useState([
+    'https://www.youtube.com/watch?v=BedeHi2HkFs',
+    'https://www.youtube.com/watch?v=iYu76OnDLhk',
+    'https://www.youtube.com/watch?v=xFzSUBzhbE4',
+  ]);
+
+  // 재생 / 정지 버튼
+  const PlayButton = () => {
+    const onClickPlayButton = () => {
+      setPlaying((prevPlaying) => !prevPlaying);
+    };
+
+    return (
+      <>
+        <button onClick={onClickPlayButton} className='bg-red-200'>
+          {playing ? 'Pause' : 'Play'}
+        </button>
+      </>
+    );
+  };
+
+  const NextButton = () => {
+    const onClickNextButton = () => {};
+
+    return (
+      <>
+        <button className='bg-blue-200'>이전</button>
+      </>
+    );
+  };
+
+  const PrevButton = () => {
+    const onClickPrevButton = () => {};
+
+    return (
+      <>
+        <button className='bg-gray-200'>이후</button>
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className='player-wrapper'>
+        <ReactPlayer
+          className='react-player'
+          width='350px' // 플레이어 크기 (가로)
+          height='200px' // 플레이어 크기 (세로)
+          url={curr}
+          // muted={false}
+
+          // width='1px' // 플레이어 크기 (가로)
+          // height='1px' // 플레이어 크기 (세로)
+          playing={playing} // 재생
+          controls={false} // 유튜브 재생 컨트롤바 노출 여부
+        />
+      </div>
+      <NextButton />
+      <PlayButton />
+      <PrevButton />
+    </>
+  );
+};
+
+export default _ReactPlayer;
