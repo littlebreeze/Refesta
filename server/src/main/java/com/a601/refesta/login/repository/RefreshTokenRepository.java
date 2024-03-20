ee84eb5e2b6fa008e5017dad3ece40913393188a
최정윤 <janet971015@naver.com>
2024-03-20 16:10:10 +0900

    🐞 fix(be): RefreshTokenRepository Query 수정

--- server/src/main/java/com/a601/refesta/login/repository/RefreshTokenRepository.java
+++ server/src/main/java/com/a601/refesta/login/repository/RefreshTokenRepository.java
@@ -2,10 +2,12 @@ package com.a601.refesta.login.repository;
 
 import com.a601.refesta.member.domain.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
-
+    
+    @Query("select rt from RefreshToken as rt where rt.isExpired=false ")
     Optional<RefreshToken> findByToken(String refreshToken);
 }
