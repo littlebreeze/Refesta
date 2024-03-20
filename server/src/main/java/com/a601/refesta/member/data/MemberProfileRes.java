36a17cf6d65acde7c1be183417fa7403780a6f99
최정윤 <janet971015@naver.com>
2024-03-20 17:45:34 +0900

    ✨ feat(be): getProfile 생성

---
+++ server/src/main/java/com/a601/refesta/member/data/MemberProfileRes.java
@@ -0,0 +1,13 @@
+package com.a601.refesta.member.data;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemberProfileRes {
+
+    private String nickname;
+    private String profileUrl;
+
+}
