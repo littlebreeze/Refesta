ecc39807d1dff2875c524aff4e9fa2723290a352
이경배 <foofoo964@naver.com>
2024-03-21 15:35:09 +0900

    📝 style(be): 불필요 import 삭제, 줄 정렬

--- server/src/main/java/com/a601/refesta/member/service/MemberService.java
+++ server/src/main/java/com/a601/refesta/member/service/MemberService.java
@@ -1,15 +1,9 @@
 package com.a601.refesta.member.service;
 
-import com.a601.refesta.artist.service.ArtistService;
 import com.a601.refesta.common.exception.CustomException;
 import com.a601.refesta.common.exception.ErrorCode;
-import com.a601.refesta.festival.repository.FestivalLineupRepository;
-import com.a601.refesta.festival.service.FestivalService;
 import com.a601.refesta.member.domain.Member;
-import com.a601.refesta.recommendation.repository.MemberArtistRepository;
-import com.a601.refesta.recommendation.repository.MemberFestivalRepository;
 import com.a601.refesta.member.repository.MemberRepository;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
