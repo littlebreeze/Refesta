798b92b656bbbe7cc6446e8d12c77d4b0b3e684b
하재률 <wofbf2@gmail.com>
2024-03-21 16:32:42 +0900

    ✨ feat(fe): 후기 삭제 기능 추가

---
+++ client/src/components/store/reviewStore.js
@@ -0,0 +1,39 @@
+import { create } from 'zustand';
+
+const useReviewStore = create((set) => ({
+  reviewList: [
+    {
+      id: 1,
+      name: '재즈 페스티벌',
+      date: new Date(),
+      location: '올림픽 공원',
+      contents: '넌ㄴ무머눔눔 좋았따 좋았따넌ㄴ무머눔눔 좋았따 좋았따넌ㄴ무머눔눔 좋았따 좋았따',
+      attachmentUrl: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201509/22/htm_2015092295240299783.jpg',
+      type: 'IMAGE',
+    },
+    {
+      id: 2,
+      name: '힙합 페스티벌',
+      date: new Date(),
+      location: '보라매 공원',
+      contents: '안 좋아따 좋아따',
+      attachmentUrl: 'https://d1b632bso7m0wd.cloudfront.net/112.mp4',
+      type: 'VIDEO',
+    },
+    {
+      id: 3,
+      name: '인디 페스티벌',
+      date: new Date(),
+      location: '막걸리 공원',
+      contents: ' 조 오 아 따 좋았따 좋았따',
+      attachmentUrl: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201509/22/htm_2015092295240299783.jpg',
+      type: 'IMAGE',
+    },
+  ],
+  removeReview: (id) =>
+    set((state) => ({
+      reviewList: state.reviewList.filter((review) => review.id !== id),
+    })),
+}));
+
+export default useReviewStore;
