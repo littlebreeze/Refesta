d51b121621a9ed0fe2266fc62bab8a5f4d219154
하재률 <wofbf2@gmail.com>
2024-03-21 17:27:23 +0900

    🌱 add(fe): 후기 store 추가

--- client/src/components/store/reviewStore.js
+++ client/src/components/store/reviewStore.js
@@ -3,37 +3,27 @@ import { create } from 'zustand';
 const useReviewStore = create((set) => ({
   reviewList: [
     {
-      id: 1,
-      name: '재즈 페스티벌',
-      date: new Date(),
-      location: '올림픽 공원',
-      contents: '넌ㄴ무머눔눔 좋았따 좋았따넌ㄴ무머눔눔 좋았따 좋았따넌ㄴ무머눔눔 좋았따 좋았따',
-      attachmentUrl: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201509/22/htm_2015092295240299783.jpg',
-      type: 'IMAGE',
+      writer: '닉네임입니다',
+      profileUrl: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201509/22/htm_2015092295240299783.jpg',
+      attachmentUrl: 'https://img.gqkorea.co.kr/gq/2023/05/style_645dce9d6929c-800x521.png',
+      mediaType: 'IMAGE',
+      contents: '나락도 락이다!!!!!!!!!!!',
     },
     {
-      id: 2,
-      name: '힙합 페스티벌',
-      date: new Date(),
-      location: '보라매 공원',
-      contents: '안 좋아따 좋아따',
+      writer: '닉네임입니다',
+      profileUrl: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201509/22/htm_2015092295240299783.jpg',
       attachmentUrl: 'https://d1b632bso7m0wd.cloudfront.net/112.mp4',
-      type: 'VIDEO',
+      mediaType: 'VIDEO',
+      contents: '영상 게시글 테스트입니다 테스트테스트',
     },
     {
-      id: 3,
-      name: '인디 페스티벌',
-      date: new Date(),
-      location: '막걸리 공원',
-      contents: ' 조 오 아 따 좋았따 좋았따',
-      attachmentUrl: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201509/22/htm_2015092295240299783.jpg',
-      type: 'IMAGE',
+      writer: '닉네임입니다',
+      profileUrl: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201509/22/htm_2015092295240299783.jpg',
+      attachmentUrl: 'https://img.gqkorea.co.kr/gq/2023/05/style_645dce9d6929c-800x521.png',
+      mediaType: 'IMAGE',
+      contents: '나123213321락325ㅅ135ㅅ도 락이다!!!!!!!!!!!',
     },
   ],
-  removeReview: (id) =>
-    set((state) => ({
-      reviewList: state.reviewList.filter((review) => review.id !== id),
-    })),
 }));
 
 export default useReviewStore;
