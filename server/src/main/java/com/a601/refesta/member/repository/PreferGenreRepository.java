778a667f5658824633859dda0c9492c99c166203
최정윤 <janet971015@naver.com>
2024-03-21 17:13:54 +0900

    ✨ feat(be): 선호 장르 관련 클래스 생성

---
+++ server/src/main/java/com/a601/refesta/member/repository/PreferGenreRepository.java
@@ -0,0 +1,10 @@
+package com.a601.refesta.member.repository;
+
+import com.a601.refesta.member.domain.join.PreferGenre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PreferGenreRepository extends JpaRepository<PreferGenre,Integer> {
+
+}
