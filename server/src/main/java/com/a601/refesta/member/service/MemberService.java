1f818de65ca16163b705ec3aa432653959ce2f7f
최정윤 <janet971015@naver.com>
2024-03-22 12:51:56 +0900

    📝 style(be): 줄 바꿈, 주석 추가

--- server/src/main/java/com/a601/refesta/member/service/MemberService.java
+++ server/src/main/java/com/a601/refesta/member/service/MemberService.java
@@ -45,6 +45,9 @@ public class MemberService {
                 .build();
     }
 
+    /**
+     * 프로필 세팅
+     */
     public void updateProfile(int memberId, String nickname, MultipartFile file) {
         Member member = getMember(memberId);
         member.setNickname(nickname);
@@ -53,7 +56,9 @@ public class MemberService {
         }
         memberRepository.save(member);
     }
-
+    /**
+     * 선호장르 저장
+     */
     public void getPreferGenre(int memberId, PreferGenreReq genres) {
         Member member = getMember(memberId);
         for (Integer genreId : genres.getPreferGenres()) {
@@ -66,9 +71,16 @@ public class MemberService {
         }
     }
 
+    /**
+     * 좋아요한 아티스트 목록
+     *
+     * @return List<LikeFestivalRes> - 페스티벌(아이디, 사진, 이름, 좋아요여부)
+     */
     public List<LikeFestivalRes> getLikeFestivals(int memberId) {
 
-        return jpaQueryFactory.select(Projections.constructor(LikeFestivalRes.class, festival.id, festival.posterUrl, festival.name, festivalLike.isLiked))
+        return jpaQueryFactory.select(Projections.constructor(
+                        LikeFestivalRes.class, festival.id, festival.posterUrl,
+                        festival.name, festivalLike.isLiked))
                 .from(festival)
                 .innerJoin(festivalLike).on(festivalLike.festival.id.eq(festival.id))
                 .innerJoin(member).on(festivalLike.member.id.eq(member.id))
@@ -77,9 +89,16 @@ public class MemberService {
                 .fetch();
     }
 
+    /**
+     * 좋아요한 아티스트 목록
+     *
+     * @return List<LikeArtistRes> - 아티스트(아이디, 사진, 이름, 좋아요여부)
+     */
     public List<LikeArtistRes> getLikeArtists(int memberId) {
 
-        return jpaQueryFactory.select(Projections.constructor(LikeArtistRes.class, artist.id, artist.pictureUrl, artist.name, artistLike.isLiked))
+        return jpaQueryFactory.select(Projections.constructor(
+                        LikeArtistRes.class, artist.id, artist.pictureUrl,
+                        artist.name, artistLike.isLiked))
                 .from(artist)
                 .innerJoin(artistLike).on(artistLike.artist.id.eq(artist.id))
                 .innerJoin(member).on(artistLike.member.id.eq(member.id))
@@ -88,9 +107,16 @@ public class MemberService {
                 .fetch();
     }
 
+    /**
+     * 내 예매내역 조회
+     *
+     * @return List<ReservationRes> - 예약 아이디, 페스티벌 (이름, 날짜, 장소)
+     */
     public List<ReservationRes> getReservations(int memberId) {
 
-        return jpaQueryFactory.select(Projections.constructor(ReservationRes.class, reservation.id, festival.name, festival.festivalDate, festival.location))
+        return jpaQueryFactory.select(Projections.constructor(
+                        ReservationRes.class, reservation.id, festival.name,
+                        festival.festivalDate, festival.location))
                 .from(festival)
                 .innerJoin(reservation).on(reservation.festival.id.eq(festival.id))
                 .innerJoin(member).on(reservation.member.id.eq(memberId))
@@ -98,7 +124,11 @@ public class MemberService {
                 .fetch();
     }
 
-
+    /**
+     * 내가 쓴 후기 조회
+     *
+     * @return List<ReviewRes> - 리뷰 (아이디, 글, 첨부파일, 파일형식), 페스티벌 (이름, 날짜, 장소)
+     */
     public List<ReviewRes> getReviews(int memberId) {
 
         return jpaQueryFactory.select(Projections.constructor(
