9431036afa4adc1a3b6a77478b695dff9a3a94dc
이지원 <dlwldnjs950@naver.com>
2024-03-22 18:15:14 +0900

    💄 design(fe): 페스티벌 전체보기 페이지 ing

--- client/src/components/home/FestivalHomeList.jsx
+++ client/src/components/home/FestivalHomeList.jsx
@@ -21,7 +21,7 @@ const FestivalHomeList = () => {
     {
       id: 3,
       posterUrl:
-        'https://ticketimage.interpark.com/Play/image/large/24/24001681_p.gif',
+        'https://scontent.cdninstagram.com/v/t39.30808-6/432650969_18424797904063392_192097024842722525_n.jpg?stp=dst-jpg_e15&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4yMDQ4eDIwNDguc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=UXelEa8niSgAX-HcGTu&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMyMTgyNzU1MTMwNzIyODk4Mw%3D%3D.2-ccb7-5&oh=00_AfADS05bJlvRv8AjcwVpiV50syCkozIDyIwU0iUP8UoHPQ&oe=660042F5&_nc_sid=10d13b',
       name: '사운드 베리 THEATER 2024 - 1일차',
       date: new Date('2024-03-16').getTime(),
     },
--- client/src/components/home/FestivalViewAllItem.jsx
+++ client/src/components/home/FestivalViewAllItem.jsx
@@ -1,4 +1,37 @@
-const FestivalViewAllItem = () => {
-  return <div>FestivalViewAllItem</div>;
+const FestivalViewAllItem = ({
+  id,
+  name,
+  date,
+  location,
+  posterUrl,
+  lineup,
+}) => {
+  return (
+    <div className='flex gap-x-4'>
+      <div className='flex items-center justify-center h-[190px] w-[190px]'>
+        <img
+          className='object-cover w-full h-full rounded-md'
+          src={posterUrl}
+        />
+      </div>
+      <div className='grid gap-y-1'>
+        <div className='text-lg'>{name}</div>
+        <div className='flex'>
+          <div>일시 | </div>
+          <div className='ml-2'>
+            {new Date(date).toLocaleDateString()}
+          </div>
+        </div>
+        <div className='flex'>
+          <div>장소 | </div>
+          <div className='ml-2'>{location}</div>
+        </div>
+        <div>
+          <div className=''>주요 라인업</div>
+          <div>{lineup}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default FestivalViewAllItem;
