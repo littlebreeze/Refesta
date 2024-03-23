9740eb6f1489d2584d62ebc83d84bb47e619868b
하재률 <wofbf2@gmail.com>
2024-03-23 19:02:39 +0900

    💄 design(fe): 아티스트 참여 페스티벌 컴포넌트 UI 구현

---
+++ client/src/components/artistDetail/FestivalList.jsx
@@ -0,0 +1,20 @@
+import FestivalItem from './FestivalItem';
+
+const FestivalList = ({ festivalList }) => {
+  if (festivalList && festivalList.length > 0) {
+    return (
+      <div className='grid grid-cols-2 gap-4 mt-4'>
+        {festivalList.map((festival, index) => (
+          <FestivalItem
+            key={index}
+            festival={festival}
+          />
+        ))}
+      </div>
+    );
+  } else {
+    return <div className='w-full mt-16 text-lg font-bold text-center'>아티스트가 참여한 페스티벌이 없습니다.</div>;
+  }
+};
+
+export default FestivalList;
