082ad6af35fd1d5bab56df4c698f73d49b600ead
littlebird <dlwldnjs950@naver.com>
2024-03-23 16:38:45 +0900

    💄 design(fe): 검색창 자동완성...모달 버전

--- client/src/components/home/ArtistHomeItem.jsx
+++ client/src/components/home/ArtistHomeItem.jsx
@@ -3,12 +3,9 @@ const ArtistHomeItem = ({ id, name, pictureUrl }) => {
     <div className='relative text-center'>
       <div className='absolute w-full h-full rounded-full bg-gradient-to-t from-stone-800 to-25%'></div>
       <div className='w-[120px] h-[200px] '>
-        <img
-          className='object-cover w-full h-full rounded-full '
-          src={pictureUrl}
-        />
+        <img className='object-cover w-full h-full rounded-full ' src={pictureUrl} />
       </div>
-      <div className='absolute w-1/2 text-white truncate bottom-2 left-7'>
+      <div className='absolute w-1/2 text-white truncate bottom-2 left-7' title={name}>
         {name}
       </div>
     </div>
--- client/src/components/home/FestivalHomeItem.jsx
+++ client/src/components/home/FestivalHomeItem.jsx
@@ -2,7 +2,9 @@ const FestivalHomeItem = ({ id, posterUrl, name, date }) => {
   return (
     <div className='w-[120px]'>
       <img className='w-full h-[160px] rounded-md' src={posterUrl} />
-      <div className='truncate'>{name}</div>
+      <div className='truncate' title={name}>
+        {name}
+      </div>
       <div className='text-sm text-zinc-500'>일시 : {new Date(date).toLocaleDateString()}</div>
     </div>
   );
