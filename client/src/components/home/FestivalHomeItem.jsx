4d33b13f02eaf0a8ba58c93a5029aeccb0d58aad
littlebird <dlwldnjs950@naver.com>
2024-03-23 12:45:43 +0900

    💄 design(fe): 전체보기 UI 완성 + 추천 페스티벌 포스터 사이즈 수정

--- client/src/components/home/FestivalHomeItem.jsx
+++ client/src/components/home/FestivalHomeItem.jsx
@@ -1,11 +1,9 @@
 const FestivalHomeItem = ({ id, posterUrl, name, date }) => {
   return (
     <div className='w-[120px]'>
-      <img className='w-full rounded-md' src={posterUrl} />
+      <img className='w-full h-[160px] rounded-md' src={posterUrl} />
       <div className='truncate'>{name}</div>
-      <div className='text-sm text-zinc-500'>
-        일시 : {new Date(date).toLocaleDateString()}
-      </div>
+      <div className='text-sm text-zinc-500'>일시 : {new Date(date).toLocaleDateString()}</div>
     </div>
   );
 };
