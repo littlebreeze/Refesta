f1536e26117baebe631188540762a346fbe8ae2b
권정훈 <dhtmxk8134@naver.com>
2024-03-25 14:52:59 +0900

    🔨 refactor(fe): 페스티벌 상세 경로 단일화(festival/:id로 단일화, festival/done/:id 삭제)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import RegisterInfo from './pages/RegisterInfoPage';
 import Home from './pages/HomePage';
 import FestivalList from './pages/FestivalListPage';
 import FestivalDetail from './pages/FestivalDetailPage';
-import FestivalDetailDone from './pages/FestivalDetailDonePage';
+// import FestivalDetailDone from './pages/FestivalDetailDonePage';
 import Notfound from './pages/NotfoundPage';
 import Google_Login from './components/start/Google_login';
 import ArtistDetail from './pages/ArtistDetailPage';
@@ -41,7 +41,7 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/festival/list' element={<FestivalList />} />
             <Route path='/festival/:id' element={<FestivalDetail />} />
-            <Route path='/festival/done/:id' element={<FestivalDetailDone />} />
+            {/* <Route path='/festival/done/:id' element={<FestivalDetailDone />} /> */}
             <Route path='/artist/:id' element={<ArtistDetail />} />
             <Route path='/reservation' element={<Reservation />} />
             <Route path='/reservation/result' element={<ReservationResult />} />
