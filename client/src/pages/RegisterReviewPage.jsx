2738850050496ab09a180c5222c01863fe266ddc
하재률 <wofbf2@gmail.com>
2024-03-25 16:58:58 +0900

    ✨ feat(fe): 후기 작성 API 연동

--- client/src/pages/RegisterReviewPage.jsx
+++ client/src/pages/RegisterReviewPage.jsx
@@ -1,12 +1,25 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import { useNavigate } from 'react-router-dom';
 
 import Header from './../components/common/Header';
 import xBtn from './../assets/x_black.png';
 import picture from './../assets/picture.png';
+import useReviewStore from '../store/reviewStore';
 
-const RegisterReview = ({ isOpen, onClose, selectedFile }) => {
+const RegisterReview = ({ isOpen, onClose, selectedFile: propSelectedFile }) => {
   const inputFileRef = useRef(null);
-  const imageRef = useRef(null);
+  const nav = useNavigate();
+  const { id } = useParams();
+  const { registerReview, addReviews } = useReviewStore();
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  const [newReview, setNewReview] = useState({});
+
+  const [selectedFile, setSelectedFile] = useState(propSelectedFile);
+  const [fileUrl, setFileUrl] = useState('');
+  const [isImage, setIsImage] = useState();
 
   // 스크롤 막기
   useEffect(() => {
@@ -21,33 +34,70 @@ const RegisterReview = ({ isOpen, onClose, selectedFile }) => {
     };
   }, [isOpen, onClose]);
 
+  // 모달 닫힐때 url정리
   useEffect(() => {
-    if (selectedFile && isOpen) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        if (imageRef.current) {
-          imageRef.current.src = e.target.result;
-        }
-      };
-      reader.readAsDataURL(selectedFile);
+    return () => {
+      if (fileUrl) {
+        URL.revokeObjectURL(fileUrl);
+        setFileUrl('');
+      }
+    };
+  }, [fileUrl]);
+
+  // newReview에 festivalId 할당
+  useEffect(() => {
+    setNewReview((prev) => ({
+      ...prev,
+      festivalId: id,
+    }));
+  }, [id]);
+
+  const handleContentsChange = (e) => {
+    const { value } = e.target;
+    setNewReview((prev) => ({
+      ...prev,
+      contents: value,
+    }));
+  };
+
+  // 넘겨받은 file 세팅
+  useEffect(() => {
+    if (propSelectedFile) {
+      setNewReview((prev) => ({
+        ...prev,
+        file: propSelectedFile,
+      }));
+      setIsImage(propSelectedFile.type.startsWith('image'));
+      setSelectedFile(propSelectedFile);
+      const newURL = URL.createObjectURL(propSelectedFile);
+      setFileUrl(newURL);
     }
-  }, [selectedFile, isOpen]);
+  }, [propSelectedFile]);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        if (imageRef.current) {
-          imageRef.current.src = e.target.result;
-        }
-      };
-      reader.readAsDataURL(file);
+      setSelectedFile(file);
+      setIsImage(file.type.startsWith('image'));
+      const newURL = URL.createObjectURL(selectedFile);
+      setFileUrl(newURL);
+
+      setNewReview((prev) => ({
+        ...prev,
+        file: file,
+      }));
     }
   };
 
   const handleImageChange = () => {
     inputFileRef.current.click();
+  };
+
+  const handleReviewSubmit = async () => {
+    await registerReview(newReview, async () => {
+      await addReviews(id);
+      onClose();
+    });
   };
 
   if (!isOpen) return null;
@@ -84,11 +134,25 @@ const RegisterReview = ({ isOpen, onClose, selectedFile }) => {
               </div>
               <div className='flex flex-col items-center'>
                 <div className='relative flex items-center justify-center w-full overflow-hidden h-72 px-7'>
-                  <img
-                    ref={imageRef}
-                    className='object-cover w-full'
-                    src=''
-                  />
+                  {isImage ? (
+                    <img
+                      key={`file-preview-${Date.now()}`}
+                      className='object-cover w-full h-full'
+                      src={fileUrl}
+                    />
+                  ) : (
+                    <ReactPlayer
+                      key={`file-preview-${Date.now()}`}
+                      url={fileUrl}
+                      muted={true}
+                      playing={isPlaying}
+                      loop={true}
+                      controls={true}
+                      width='100%'
+                      height='100%'
+                      className='bg-gray-800'
+                    />
+                  )}
                   <div
                     className='absolute flex items-center justify-center w-12 h-12 bg-gray-300 rounded-full bottom-3 right-10'
                     onClick={handleImageChange}
@@ -103,7 +167,7 @@ const RegisterReview = ({ isOpen, onClose, selectedFile }) => {
                     className='hidden'
                     type='file'
                     name='file'
-                    accept='image/*'
+                    accept='image/*, video/*'
                     onChange={handleFileChange}
                   />
                 </div>
@@ -112,8 +176,12 @@ const RegisterReview = ({ isOpen, onClose, selectedFile }) => {
                   name=''
                   id=''
                   placeholder='문구 작성...'
+                  onChange={handleContentsChange}
                 ></textarea>
-                <button className='flex items-center justify-center w-11/12 mt-3 text-white rounded-md bg-ourIndigo h-14'>
+                <button
+                  className='flex items-center justify-center w-11/12 mt-3 text-white rounded-md bg-ourIndigo h-14'
+                  onClick={handleReviewSubmit}
+                >
                   등록하기
                 </button>
               </div>
