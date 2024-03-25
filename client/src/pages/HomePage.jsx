6c5d901abf5f71603c31f341637b14694f8f4f38
이지원 <dlwldnjs950@naver.com>
2024-03-25 13:53:00 +0900

    ✨ feat(fe): 요청 데이터로 렌더링 (요청주소 수정 필요)

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,15 +4,30 @@ import FestivalHomeList from '../components/home/FestivalHomeList';
 import SetListHomeList from '../components/home/SetListHomeList';
 
 import instance from '../util/token_interceptor';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
-  const test = async () => {
+  const [festivalData, setFestivalData] = useState([]);
+  const [setListData, setSetListData] = useState([]);
+
+  // 추천 페스티벌 정보 요청
+  const getRecommendFestival = async () => {
+    //const response = await instance.get('recommendations/festivals', {
     const response = await instance.get('festivals/2');
+    setFestivalData([
+      response.data.data,
+      response.data.data,
+      response.data.data,
+    ]);
+    setSetListData([
+      response.data.data,
+      response.data.data,
+      response.data.data,
+    ]);
   };
 
   useEffect(() => {
-    test();
+    getRecommendFestival();
   }, []);
 
   return (
@@ -20,13 +35,13 @@ const Home = () => {
       <div>
         <Carousel />
         <div className='pt-7 pl-7'>
-          <FestivalHomeList />
+          <FestivalHomeList festivalData={festivalData} />
         </div>
         <div className='pt-7 pl-7'>
           <ArtistHomeList />
         </div>
         <div className='py-7 pl-7'>
-          <SetListHomeList />
+          <SetListHomeList setListData={setListData} />
         </div>
       </div>
     </div>
