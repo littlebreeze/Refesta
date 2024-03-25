394b7b5aea819ad0dcf9f3b8ea457607ab2d537a
이지원 <dlwldnjs950@naver.com>
2024-03-25 13:59:01 +0900

    ✨ feat(fe): 각아이템 눌렀을 때, 페이지 이동

--- client/src/components/home/ArtistHomeItem.jsx
+++ client/src/components/home/ArtistHomeItem.jsx
@@ -1,11 +1,24 @@
+import { useNavigate } from 'react-router';
+
 const ArtistHomeItem = ({ id, name, pictureUrl }) => {
+  const nav = useNavigate();
+
+  const onClickItem = () => {
+    nav(`/artist/${id}`);
+  };
   return (
-    <div className='relative text-center'>
+    <div className='relative text-center' onClick={onClickItem}>
       <div className='absolute w-full h-full rounded-full bg-gradient-to-t from-stone-800 to-25%'></div>
       <div className='w-[120px] h-[200px] '>
-        <img className='object-cover w-full h-full rounded-full ' src={pictureUrl} />
+        <img
+          className='object-cover w-full h-full rounded-full '
+          src={pictureUrl}
+        />
       </div>
-      <div className='absolute w-1/2 text-white truncate bottom-2 left-7' title={name}>
+      <div
+        className='absolute w-1/2 text-white truncate bottom-2 left-7'
+        title={name}
+      >
         {name}
       </div>
     </div>
--- client/src/components/home/FestivalHomeItem.jsx
+++ client/src/components/home/FestivalHomeItem.jsx
@@ -1,11 +1,20 @@
+import { useNavigate } from 'react-router';
+
 const FestivalHomeItem = ({ id, posterUrl, name, date }) => {
+  const nav = useNavigate();
+
+  const onClickItem = () => {
+    nav(`/festival/${id}`);
+  };
   return (
-    <div className='w-[120px]'>
+    <div className='w-[120px]' onClick={onClickItem}>
       <img className='w-full h-[160px] rounded-md' src={posterUrl} />
       <div className='truncate' title={name}>
         {name}
       </div>
-      <div className='text-sm text-zinc-500'>일시 : {new Date(date).toLocaleDateString()}</div>
+      <div className='text-sm text-zinc-500'>
+        일시 : {new Date(date).toLocaleDateString()}
+      </div>
     </div>
   );
 };
