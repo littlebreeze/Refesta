f9113bbdca1269b69e9b9956a1eea36bd5b87184
하재률 <wofbf2@gmail.com>
2024-03-27 14:32:53 +0900

    ✨ feat(fe): 좋아요한 아티스트/페스티벌 조회 API 연동

--- client/src/components/mypage/LikeItem.jsx
+++ client/src/components/mypage/LikeItem.jsx
@@ -1,27 +1,44 @@
+import useArtistStore from '../../store/artistStore';
+import useLikeStore from '../../store/likeStore';
+
 import heartFull from './../../assets/heart_full.png';
 import heart from './../../assets/heart.png';
 
-const LikeItem = ({ artist }) => {
-  console.log(artist);
-  const { id, name, url, isLike } = artist;
+const LikeItem = ({ content, onClick }) => {
+  const { id, name, url, like, type } = content;
+  const { updateLike } = useArtistStore();
+  const { toggleLike } = useLikeStore();
+
+  const handleLikeBtn = () => {
+    if (type === 'artist') {
+      console.log(id, type);
+      toggleLike(id);
+      updateLike(id);
+    }
+  };
+
   return (
-    <div className="relative flex flex-col w-full overflow-hidden rounded-2xl">
-      <div className="h-44">
+    <div
+      className='relative flex flex-col w-full overflow-hidden rounded-2xl'
+      onClick={onClick}
+    >
+      <div className='h-44'>
         <img
-          className="object-cover w-full h-full"
+          className='object-cover w-full h-full'
           src={url}
         />
       </div>
-      <div className="px-3 py-2 text-xs font-bold text-center truncate bg-white">{name}</div>
+      <div className='px-3 py-2 text-xs font-bold text-center truncate bg-white'>{name}</div>
       <div
-        className="absolute h-12 py-2.5 bottom-8 right-2"
-        onClick={() => {
-          console.log(id);
+        className='absolute h-12 py-2.5 bottom-8 right-2'
+        onClick={(e) => {
+          e.stopPropagation();
+          handleLikeBtn();
         }}
       >
         <img
-          src={isLike ? `${heartFull}` : `${heart}`}
-          className="h-full"
+          src={like ? `${heartFull}` : `${heart}`}
+          className='h-full'
         />
       </div>
     </div>
