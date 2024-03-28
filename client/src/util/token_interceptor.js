dfbc3dc2de3b8ba412c368f56d1475f1bf24add1
강지헌 <wlgjs0458@naver.com>
2024-03-28 10:27:55 +0900

    🐞 fix: http 주소 수정

--- client/src/util/token_interceptor.js
+++ client/src/util/token_interceptor.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_API_SERVER,
   headers: {
-    'Access-Control-Allow-Origin': 'http://http://j10a601.p.ssafy.io/8081',
+    'Access-Control-Allow-Origin': 'https://j10a601.p.ssafy.io/8081',
     'Access-Control-Allow-Credentials': 'true',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
   },
