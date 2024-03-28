40bc6d64ab92fc529c9830daa0f9e6425e4541e7
최정윤 <janet971015@naver.com>
2024-03-28 12:53:12 +0900

    🐞 fix(be): 결제 화면 url 변경

--- server/src/main/java/com/a601/refesta/reservation/data/PayRes.java
+++ server/src/main/java/com/a601/refesta/reservation/data/PayRes.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class PayRes {
 
     private String tid;
-    private String next_redirect_pc_url;
+    private String next_redirect_mobile_url;
     private String created_at;
 
 }
