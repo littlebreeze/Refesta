b592fde406cb0bc423e459307b7e8f6c4b45379e
권정훈 <dhtmxk8134@naver.com>
2024-03-29 17:29:55 +0900

    📝 style(fe): 코드 주석

--- client/src/components/festivalDetail/SetListPlayer.jsx
+++ client/src/components/festivalDetail/SetListPlayer.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import useSetListStore from '@store/setListStore';
 
+import instance from '@util/token_interceptor';
 import ReactPlayer from 'react-player';
 
 import play_btn from '@assets/play_btn.png';
@@ -10,37 +11,23 @@ import next_btn from '@assets/next_btn.png';
 import default_album_poster from '@assets/default_album_poster.jpg';
 
 const SetListPlayer = () => {
-  const {
-    lineupList,
-    addLineupList,
-    selectedLineupList,
-    setSelectedLineupList,
-    songInfoMap,
-    addSongInfoMap,
-    sortedSongInfoMap,
-    sortSongInfoMapByLineupList,
-    selectedSongInfoMap,
-    setSelectedSongInfoMap,
-    playing,
-    setPlaying,
-    currSong,
-    setCurrSong,
-    currSongList,
-    setCurrSongList,
-    currSinger,
-  } = useSetListStore();
+  const { playing, setPlaying, currSong, setCurrSong, currSongList } = useSetListStore();
   const [currSongIndex, setCurrSongIndex] = useState(0);
 
   useEffect(() => {
     if (currSongList && currSongList.length > 0) {
       const index = currSongList.findIndex((song) => song === currSong);
-      setCurrSongIndex(index >= 0 ? index : 0); // 현재 곡이 있는 경우 해당 인덱스로 설정
+      setCurrSongIndex(index >= 0 ? index : 0);
     }
+    console.log('현재곡', currSong.title);
   }, [currSongList, currSong]);
 
-  // 재생&일시정지
+  // 재생 & 일시정지
   const onClickPlayButton = () => {
     setPlaying(!playing);
+    if (!playing) {
+      console.log('현재곡', currSong.title);
+    }
   };
 
   // 이전 곡 재생
@@ -67,8 +54,16 @@ const SetListPlayer = () => {
     }
   };
 
-  console.log('currSongList', currSongList);
-  console.log('currSong', currSong);
+  const updateSongPreference = async () => {
+    try {
+      const response = await instance.patch(`recommendations/songs/${currSong.id}`);
+      // if (response.data.status === 'success') {
+      //   console.log(response.data);
+      // }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
     <div className='flex justify-between mx-4 bg-white rounded-md'>
@@ -88,9 +83,13 @@ const SetListPlayer = () => {
           className='react-player'
           width='0px' // 플레이어 크기 (가로)
           height='0px' // 플레이어 크기 (세로)
-          url={currSong.audioUrl}
+          url={currSong.audioUrl} // 현재 재생 곡
           playing={playing} // 재생
-          controls={false} // 유튜브 재생 컨트롤바 노출 여부
+          onProgress={(progress) => {
+            if (parseInt(progress.playedSeconds) !== 0 && parseInt(progress.playedSeconds) % 30 === 0) {
+              updateSongPreference(); // 선호도 증가
+            }
+          }}
         />
       </div>
       <div className='flex justify-between pr-2 my-auto mr- min-w-20'>
