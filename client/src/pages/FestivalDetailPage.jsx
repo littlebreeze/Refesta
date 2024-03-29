f063eed1dba431296f740cd5e42897ae5c14e47e
하재률 <wofbf2@gmail.com>
2024-03-29 17:50:55 +0900

    🔨 refactor(fe): 추천 데이터 추가 (페스티벌, 아티스트 조회)

--- client/src/pages/FestivalDetailPage.jsx
+++ client/src/pages/FestivalDetailPage.jsx
@@ -46,6 +46,17 @@ const FestivalDetailPage = () => {
       }
     };
 
+    // 페스티벌 상세 페이지 접근 데이터 제공
+    const increaseFestivalViewCount = async () => {
+      try {
+        const res = await instance.patch(`festivals/${id}/views`);
+        console.log(res);
+      } catch (e) {
+        console.error('Error:', e);
+      }
+    };
+
+    increaseFestivalViewCount();
     getFestivalInfoData();
   }, []);
 
