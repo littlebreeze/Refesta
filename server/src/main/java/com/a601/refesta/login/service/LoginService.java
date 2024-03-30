878d15d4ad60fd621239cb4c64aea9be4fa51fca
최정윤 <janet971015@naver.com>
2024-03-30 22:31:18 +0900

    ✨ feat(be): 구글accessToken 레디스 저장 로직 구현

--- server/src/main/java/com/a601/refesta/login/service/LoginService.java
+++ server/src/main/java/com/a601/refesta/login/service/LoginService.java
@@ -3,14 +3,10 @@ package com.a601.refesta.login.service;
 import com.a601.refesta.common.exception.CustomException;
 import com.a601.refesta.common.exception.ErrorCode;
 import com.a601.refesta.common.jwt.TokenProvider;
-import com.a601.refesta.login.data.GoogleOAuthTokenRes;
-import com.a601.refesta.login.data.GoogleUserInfoRes;
-import com.a601.refesta.login.data.MemberDetail;
-import com.a601.refesta.login.data.OauthTokenRes;
+import com.a601.refesta.login.data.*;
 import com.a601.refesta.login.repository.RefreshTokenRepository;
 import com.a601.refesta.member.data.MemberRole;
 import com.a601.refesta.member.domain.Member;
-import com.a601.refesta.login.data.RefreshToken;
 import com.a601.refesta.member.repository.MemberRepository;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +30,7 @@ public class LoginService {
     private final MemberRepository memberRepository;
     private final RefreshTokenService refreshTokenService;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final GoogleAccessTokenService googleAccessTokenService;
 
     private final String GRANT_TYPE = "authorization_code";
 
@@ -103,15 +100,16 @@ public class LoginService {
     public OauthTokenRes getAccessTokenJsonData(String code) {
         //코드로 토큰받기
         GoogleOAuthTokenRes oauthTokenData = getTokenbyCode(code);
-        System.out.println("구글 access토큰"+oauthTokenData.getAccess_token());
+        System.out.println("구글 access토큰" + oauthTokenData.getAccess_token());
+
         //토큰으로 사용자 정보 받기
         GoogleUserInfoRes googleUserInfoRes = getUserInfoByToken(oauthTokenData.getAccess_token());
 
         //받아온 사용자 정보(로그인/회원가입한 유저)로 우리 토큰 만들기
-        return generateTokenbyUserInfo(googleUserInfoRes);
+        return generateTokenbyUserInfo(googleUserInfoRes, oauthTokenData.getAccess_token());
     }
 
-    private OauthTokenRes generateTokenbyUserInfo(GoogleUserInfoRes googleUserInfoRes) {
+    private OauthTokenRes generateTokenbyUserInfo(GoogleUserInfoRes googleUserInfoRes, String googleAccessToken) {
         boolean signUp = false;
         if (memberRepository.findByGoogleId(googleUserInfoRes.getId()) == null) { //우리 회원이 아니면
             Member newMember = Member.builder()
@@ -123,7 +121,13 @@ public class LoginService {
                     .build();
             memberRepository.save(newMember);
             signUp = true;
+
+
         }
+        //회원가입: 레디스에 구글 accessToken 저장 test 위해서 밖으로 빼놓음
+        googleAccessTokenService.saveTokenInfo(
+                memberRepository.findByGoogleId(googleUserInfoRes.getId()).getId(), googleAccessToken
+        );
 
         Member member = memberRepository.findByGoogleId(googleUserInfoRes.getId());
         if (member == null) {
