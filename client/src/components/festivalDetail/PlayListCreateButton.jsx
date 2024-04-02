69552bbfe95f38f26ed7f76d5a1ba0852c28ab55
권정훈 <dhtmxk8134@naver.com>
2024-04-02 09:58:50 +0900

    ✨ feat(fe): 유튜브 플레이리스트 저장 기능

--- client/src/components/festivalDetail/PlayListCreateButton.jsx
+++ client/src/components/festivalDetail/PlayListCreateButton.jsx
@@ -4,22 +4,23 @@ import useFestivalInfoStore from '@store/festivalInfoStore';
 import instance from '@util/token_interceptor';
 
 // 셋리스트 재생목록의 각 노래
-const PlayListCreateButton = ({ song }) => {
+const PlayListCreateButton = () => {
   const { selectedSongInfoMap, currSongList } = useSetListStore();
   const { festivalInfoData } = useFestivalInfoStore();
 
   const createYoutubePlaylist = async () => {
     try {
-      console.log('현재페스티벌', festivalInfoData);
-      console.log('현재재생노래', selectedSongInfoMap);
-      console.log('현재재생목록', currSongList);
-
-      //   const response = await instance.post(`festivals/playlists`);
-      //   if (response.data.status === 'success') {
-      //     // 성공 모달 띄우기
-      //   } else {
-      //     // 실패 모달 띄우기
-      //   }
+      const audioUrlList = currSongList.map((song) => song.audioUrl);
+      const response = await instance.post(`festivals/playlists`, {
+        festivalName: `${festivalInfoData.name}`,
+        audioUrlList: audioUrlList,
+      });
+      if (response.data.status === 'success') {
+        console.log('성공', response.data.data);
+        // 성공 모달 띄우기
+      } else {
+        // 실패 모달 띄우기
+      }
     } catch (error) {
       console.error('Error:', error);
     }
